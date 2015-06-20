b50e02869af72718c0ce9f3c650768ca3bff2363
antham <hamonanth@gmail.com>
2015-06-20 09:38:01 +0200

    Remove unused methods at the moment

--- mailbox/mailbox.go
+++ mailbox/mailbox.go
@@ -10,22 +10,11 @@ func NewMailBox() {
 
 }
 
-func (m *MailBox) Flush() bool {
-
-}
 
 func (m *MailBox) GetMails(emails string, limit int) bool {
 
-}
 
-func (m *MailBox) GetMail() bool {
 
-}
 
-func (m *Mail) Delete() bool {
-
-}
-
-func (m *Mail) GetAlias() string {
 
 }
