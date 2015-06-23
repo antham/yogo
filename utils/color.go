667e503fb961cbe4575abc8bb3d22fd8180e77bd
antham <hamonanth@gmail.com>
2015-06-23 21:59:28 +0200

    Create utils package and move color handling in it

---
+++ utils/color.go
@@ -0,0 +1,8 @@
+package utils
+
+import "github.com/fatih/color"
+
+var Blue = color.New(color.FgBlue).SprintFunc()
+var Cyan = color.New(color.FgCyan).SprintFunc()
+var Yellow = color.New(color.FgYellow).SprintFunc()
+var Magenta = color.New(color.FgMagenta).SprintFunc()
--- view/view.go
+++ view/view.go
@@ -1,13 +1,8 @@
 package view
 
-import "github.com/fatih/color"
 import "github.com/antham/yogo/mailbox"
+import "github.com/antham/yogo/utils"
 import "fmt"
-
-var blue = color.New(color.FgBlue).SprintFunc()
-var cyan = color.New(color.FgCyan).SprintFunc()
-var yellow = color.New(color.FgYellow).SprintFunc()
-var magenta = color.New(color.FgMagenta).SprintFunc()
 
 func OutputMails(mails []*mailbox.Mail) {
 	for _, mail := range mails {
@@ -16,17 +11,17 @@ func OutputMails(mails []*mailbox.Mail) {
 }
 
 func OutputMail(mail *mailbox.Mail) {
-	fmt.Printf("%s\n%s\n", yellow(mail.Title), cyan(mail.SumUp))
+	fmt.Printf("%s\n%s\n", utils.Yellow(mail.Title), utils.Cyan(mail.SumUp))
 	fmt.Printf("---\n")
 }
 
 func OutputCompleteMail(mail *mailbox.Mail) {
 
 	fmt.Printf("---\n")
-	fmt.Printf("From  : %s <%s>\n", magenta(mail.FromString), magenta(mail.FromMail))
-	fmt.Printf("Title : %s\n", yellow(mail.Title))
-	fmt.Printf("Date  : %s\n", blue(mail.Date.Format("2006-01-02 15:04")))
+	fmt.Printf("From  : %s <%s>\n", utils.Magenta(mail.FromString), utils.Magenta(mail.FromMail))
+	fmt.Printf("Title : %s\n", utils.Yellow(mail.Title))
+	fmt.Printf("Date  : %s\n", utils.Blue(mail.Date.Format("2006-01-02 15:04")))
 	fmt.Printf("---\n")
-	fmt.Printf(cyan(mail.Body))
+	fmt.Printf(utils.Cyan(mail.Body))
 	fmt.Printf("\n---\n")
 }
