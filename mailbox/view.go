f2d691387b99bd3126defd21e34ed20b9e7f47a8
antham <hamonanth@gmail.com>
2015-06-27 12:05:29 +0200

    Add mail numbering in mailbox

--- mailbox/view.go
+++ mailbox/view.go
@@ -3,16 +3,23 @@ package mailbox
 import "github.com/antham/yogo/utils"
 import "github.com/antham/yogo/mail"
 import "fmt"
+import "strings"
+import "strconv"
 
 func Render(mailbox *Mailbox) {
-	for _, mail := range mailbox.GetAll() {
-		renderMail(mail)
+	mails := mailbox.GetAll()
+
+	for index, mail := range mails {
+		renderMail(mail, index, len(mails))
 	}
 }
 
-func renderMail(mail *mail.Mail) {
-	fmt.Printf("%s\n%s\n", utils.Yellow(mail.Title), utils.Cyan(mail.SumUp))
-	fmt.Printf("---\n")
+func renderMail(mail *mail.Mail, index int, totalMails int) {
+	totalSpaces := strings.Repeat(" ", len(strconv.Itoa(totalMails)))
+	remainingSpaces := strings.Repeat(" ", len(strconv.Itoa(totalMails))-len(strconv.Itoa(index+1)))
+
+	fmt.Printf(" %s%v %s\n", utils.Green(index+1), remainingSpaces, utils.Yellow(mail.Title))
+	fmt.Printf(" %v %s\n\n", totalSpaces, utils.Cyan(mail.SumUp))
 }
 
 func RenderMessage(string string) {
