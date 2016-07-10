a525cb7bccdc21b7d589bde836c2596d5ca1437a
antham <hamonanth@gmail.com>
2016-07-11 01:12:35 +0200

    feat(version) : add

--- cmd/version.go
+++ cmd/version.go
@@ -1,17 +1,17 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
+
+var version = "0.0.0"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "App version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+		info("v" + version)
 	},
 }
 
