4d8a78cca385f3870aea18135ba167d9bd87ac4c
antham <hamonanth@gmail.com>
2016-07-11 00:29:15 +0200

    feat(cmd/inbox_delete) : add email deletion

---
+++ cmd/inbox_delete.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/antham/yogo/inbox"
+)
+
+// inboxDeleteCmd delete an email in inbox
+var inboxDeleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete email at given position in inbox",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		identifier, offset := parseMailAndOffsetArgs(args)
+
+		in, err := inbox.ParseInboxPages(identifier, offset)
+
+		if err != nil {
+			perror(err)
+
+			errorExit()
+		}
+
+		checkOffset(in.Count(), offset)
+
+		in.Remove(offset - 1)
+		success(fmt.Sprintf(`Email "%d" successfully deleted`, offset))
+	},
+}
+
+func init() {
+	inboxCmd.AddCommand(inboxDeleteCmd)
+}
