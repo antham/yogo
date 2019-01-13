e900201812e5d6c115e2734fc4f781e1a5355276
Anthony HAMON <anthony.hamon@iadvize.com>
2019-01-13 14:37:35 +0100

    Fix issue when fetching single email

--- inbox/util.go
+++ inbox/util.go
@@ -1,6 +1,9 @@
 package inbox
 
 import (
+	"bytes"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
@@ -9,6 +12,43 @@ import (
 
 var send = func(URL string) {
 	http.Get(URL)
+}
+
+var buildReader = func(method string, URL string, headers map[string]string, body io.Reader) (io.Reader, error) {
+	r, err := http.NewRequest(method, URL, body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range headers {
+		r.Header.Add(k, v)
+	}
+
+	c := http.Client{}
+	res, err := c.Do(r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes.NewBuffer(b), nil
+}
+
+var fetchFromReader = func(r io.Reader) (*goquery.Document, error) {
+	doc, err := goquery.NewDocumentFromReader(r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, err
 }
 
 var fetchURL = func(URL string) (*goquery.Document, error) {
