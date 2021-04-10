2c1ac288481e2da45d93301f0991638512007aa5
Anthony HAMON <hamon.anth@gmail.com>
2021-04-10 21:45:45 +0200

    Use a lighter color to render the date

--- cmd/inbox_view.go
+++ cmd/inbox_view.go
@@ -60,7 +60,7 @@ func renderMail(mail *inbox.Mail) {
 		output(fmt.Sprintf("From  : %s <%s>\n", color.MagentaString(mail.Sender.Name), color.MagentaString(mail.Sender.Mail)))
 	}
 	output(fmt.Sprintf("Title : %s\n", color.YellowString(mail.Title)))
-	output(fmt.Sprintf("Date  : %s\n", color.BlueString(mail.Date.Format("2006-01-02 15:04"))))
+	output(fmt.Sprintf("Date  : %s\n", color.GreenString(mail.Date.Format("2006-01-02 15:04"))))
 	output("---\n")
 	output(color.CyanString(mail.Body))
 	output("\n---\n")
