2559a23f17bb8a322e3459676cc69ba281e0519d
Anthony HAMON <hamon.anth@gmail.com>
2022-08-14 00:08:36 +0200

    Add tests

--- inbox/mail_test.go
+++ inbox/mail_test.go
@@ -1,6 +1,7 @@
 package inbox
 
 import (
+	"errors"
 	"os"
 	"testing"
 
@@ -28,24 +29,58 @@ func getDoc(t *testing.T, filename string) *goquery.Document {
 }
 
 func TestParseFrom(t *testing.T) {
-	name, mail := parseFrom(`John Doe <john.doe@unknown.com>`)
-	assert.Equal(t, "John Doe", name)
-	assert.Equal(t, "john.doe@unknown.com", mail)
+	type scenario struct {
+		name        string
+		fromArg     string
+		resultEmail string
+		resultName  string
+	}
 
-	name, mail = parseFrom(`<john.doe@unknown.com>`)
-	assert.Equal(t, "", name)
-	assert.Equal(t, "john.doe@unknown.com", mail)
+	scenarios := []scenario{
+		{
+			name:        "parse name and email",
+			fromArg:     "John Doe <john.doe@unknown.com>",
+			resultName:  "John Doe",
+			resultEmail: "john.doe@unknown.com",
+		},
+		{
+			name: "parse name and email with spaces",
+			fromArg: `Liana
+                <AnnaMartinezpisea@lionspest.com.au>`,
+			resultName:  "Liana",
+			resultEmail: "AnnaMartinezpisea@lionspest.com.au",
+		},
+		{
+			name:        "parse email only",
+			fromArg:     "<john.doe@unknown.com>",
+			resultName:  "",
+			resultEmail: "john.doe@unknown.com",
+		},
+		{
+			name:        "no email nor name to parse",
+			fromArg:     "",
+			resultName:  "",
+			resultEmail: "",
+		},
+	}
 
-	name, mail = parseFrom(`Liana
-                <AnnaMartinezpisea@lionspest.com.au>`)
-	assert.Equal(t, "Liana", name)
-	assert.Equal(t, "AnnaMartinezpisea@lionspest.com.au", mail)
+	for _, s := range scenarios {
+		s := s
+		t.Run(s.name, func(t *testing.T) {
+			t.Parallel()
+			name, mail := parseFrom(s.fromArg)
+			assert.Equal(t, s.resultName, name)
+			assert.Equal(t, s.resultEmail, mail)
+		})
+	}
 }
 
 func TestParseDate(t *testing.T) {
 	date := parseDate("Sunday, June 13, 2021 8:57:08 PM")
-
 	assert.Equal(t, "2021-06-13 20:57:08 +0000 UTC", date.String(), "Must parse date")
+
+	date = parseDate("whatever")
+	assert.Empty(t, date)
 }
 
 func TestParseMail(t *testing.T) {
@@ -67,4 +102,37 @@ Will you come to me on the weekend?
 
 *s ho Todd aquaman bullock falcone jester chase croc doom swamp sinestro hangman fairchild nocturna hangman creeper hangman caird aquaman kane barrow. w p Clench chill green canary metallo face robin shrike hatter riddler gleeson justice rumor batarang kane lucius ragman fox grey batmobile. ho Night gleeson oswald cluemaster abattoir ragman gleeson oswald elongated batmobile face quinn abbott clayface moth knight prey knight atkins killer? to* ( https://exteleer.page.link/kjcS )`, mail.Body, "Must return mail body")
 	assert.Equal(t, "2021-06-13 20:57:08 +0000 UTC", mail.Date.String(), "Must return mail date")
+}
+
+func TestParseHTML(t *testing.T) {
+	type scenario struct {
+		name         string
+		contentArg   string
+		errorArg     error
+		resultString string
+	}
+
+	scenarios := []scenario{
+		{
+			name:         "error provided is not nil",
+			contentArg:   "",
+			errorArg:     errors.New("an error occurred"),
+			resultString: "",
+		},
+		{
+			name:         "extract text from HTML",
+			contentArg:   "<html>text</html>",
+			errorArg:     nil,
+			resultString: "text",
+		},
+	}
+
+	for _, s := range scenarios {
+		s := s
+		t.Run(s.name, func(t *testing.T) {
+			t.Parallel()
+			str := parseHTML(s.contentArg, s.errorArg)
+			assert.Equal(t, s.resultString, str)
+		})
+	}
 }
