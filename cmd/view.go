a64c3fbacb150d7a811d90cb6703f9e373c1c629
Anthony HAMON <hamon.anth@gmail.com>
2023-03-21 21:49:14 +0100

    Rename files

--- cmd/view.go
+++ cmd/view.go
@@ -1,15 +1,99 @@
 package cmd
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"text/template"
+
 	"github.com/fatih/color"
+
+	"github.com/antham/yogo/inbox"
 )
 
-// success outputs a green successful message
-func success(message string) string {
-	return color.GreenString(message)
+var ErrSomethingWrongOccurred = errors.New("something wrong occurred")
+
+func computeInboxMailOutput(in Inbox) (string, error) {
+	JSON, err := computeJSONOutput(in)
+	if err != nil {
+		return "", err
+	}
+	if JSON != nil {
+		return *JSON, nil
+	}
+
+	if in.Count() == 0 {
+		return "", errors.New("inbox is empty")
+	}
+
+	output := ""
+	for index, mail := range in.GetMails() {
+		var spam string
+		if mail.IsSPAM {
+			spam = " [SPAM]"
+		}
+		output = output + fmt.Sprintf(" %s %s%s%s\n", color.GreenString(fmt.Sprintf("%d", index+1)), color.YellowString(mail.Sender.Mail), color.YellowString(mail.Sender.Name), color.RedString(spam))
+		output = output + fmt.Sprintf(" %s\n\n", color.CyanString(mail.Title))
+	}
+	return output, nil
+}
+
+func computeMailOutput(mail *inbox.Mail) (string, error) {
+	JSON, err := computeJSONOutput(*mail)
+	if err != nil {
+		return "", err
+	}
+	if JSON != nil {
+		return *JSON, nil
+	}
+
+	const noDataToDisplayMsg = "<No data to display>"
+	output := "---\n"
+
+	switch {
+	case mail.Sender.Mail == "":
+		output = output + fmt.Sprintf("From  : %s\n", color.MagentaString(noDataToDisplayMsg))
+	case mail.Sender.Name == "":
+		output = output + fmt.Sprintf("From  : %s\n", color.MagentaString(mail.Sender.Mail))
+	case mail.Sender.Name != "":
+		output = output + fmt.Sprintf("From  : %s <%s>\n", color.MagentaString(mail.Sender.Name), color.MagentaString(mail.Sender.Mail))
+	}
+
+	output = output + fmt.Sprintf("Title : %s\n", color.YellowString(mail.Title))
+	if mail.Date == nil {
+		output = output + fmt.Sprintf("Date  : %s\n", color.GreenString(noDataToDisplayMsg))
+	} else {
+		output = output + fmt.Sprintf("Date  : %s\n", color.GreenString(mail.Date.Format("2006-01-02 15:04")))
+	}
+	output = output + "---\n"
+	if mail.Body == "" {
+		output = output + color.CyanString(noDataToDisplayMsg)
+	} else {
+		output = output + color.CyanString(mail.Body)
+	}
+	output = output + "\n---\n"
+	return output, nil
+}
+
+func computeJSONOutput(d interface{}) (*string, error) {
+	if dumpJSON {
+		data, err := json.Marshal(d)
+		if err != nil {
+			return nil, ErrSomethingWrongOccurred
+		}
+		s := string(data)
+		return &s, nil
+	}
+	return nil, nil
 }
 
 // info outputs a blue info message
 func info(message string) string {
 	return color.CyanString(message)
+}
+
+// success outputs a green successful message
+func success(message string) string {
+	return color.GreenString(message)
 }
