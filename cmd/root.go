db9f6a868416da39aa03ddfe1a5d8a25bffb9299
Anthony HAMON <hamon.anth@gmail.com>
2023-03-22 00:37:02 +0100

    Update README description

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ var dumpJSON = false
 var RootCmd = &cobra.Command{
 	Use:   "yogo",
 	Short: "Interact with yopmail from command-line",
-	Long:  `Check yopmail mails and inboxes from command line.`,
+	Long:  `Check yopmail mails from command line.`,
 }
 
 func Execute() {
