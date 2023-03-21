3029f5fe3300936d5f9e384c7672a7e123868526
Anthony HAMON <hamon.anth@gmail.com>
2023-03-21 21:42:22 +0100

    Simplify

--- cmd/inbox.go
+++ cmd/inbox.go
@@ -10,10 +10,7 @@ var inboxCmd = &cobra.Command{
 	Use:   "inbox",
 	Short: "Handle inbox messages",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if err := cmd.Help(); err != nil {
-			return err
-		}
-		return nil
+		return cmd.Help()
 	},
 }
 
